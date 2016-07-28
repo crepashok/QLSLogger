30ee715674d5b8b8a325aeafcd11e7aa1a536f91
Pasha <pasha@qlicks.nl>
2016-07-28 14:58:05 +0300

    New file structure

--- Example/Pods/Target Support Files/Pods-QLSLogger_Example/Pods-QLSLogger_Example-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_QLSLogger_ExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_QLSLogger_ExampleVersionString[];
-
--- Example/Pods/Target Support Files/QLSLogger/QLSLogger-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_QLSLogger : NSObject
-@end
-@implementation PodsDummy_QLSLogger
-@end
