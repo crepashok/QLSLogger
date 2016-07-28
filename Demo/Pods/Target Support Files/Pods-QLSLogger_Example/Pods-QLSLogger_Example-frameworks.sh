9242664a746e37929fe2788a0c4eacb59521701f
Pasha <pasha@qlicks.nl>
2016-07-28 15:17:30 +0300

    File structure

--- Demo/Pods/Target Support Files/Pods-QLSLogger_Example/Pods-QLSLogger_Example-frameworks.sh
+++ Demo/Pods/Target Support Files/Pods-QLSLogger_Example/Pods-QLSLogger_Example-frameworks.sh
@@ -85,9 +85,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/CocoaLumberjack/CocoaLumberjack.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/QLSLogger/QLSLogger.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftHEXColors/SwiftHEXColors.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/CocoaLumberjack/CocoaLumberjack.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/QLSLogger/QLSLogger.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftHEXColors/SwiftHEXColors.framework"
 fi
---
+++ Demo/Pods/Target Support Files/QLSLogger/QLSLogger-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double QLSLoggerVersionNumber;
+FOUNDATION_EXPORT const unsigned char QLSLoggerVersionString[];
+
